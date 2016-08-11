b9ce3f86d479e1fc849f2d5a751cf3669ee9312c
Young Woo <ywoo@umich.edu>
2016-08-11 14:51:49 -0400

    add bryan mytko files

---
+++ router/routes/home.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res){
+  if(!req.session.user){
+    res.redirect('sessions/new');
+  } else {
+    res.render('index', { 'email': req.session.user.email });
+  }
+});
+
+module.exports = router;
